b667717ce756dd71addb90fd8dda8be4e0459630
pche64 <pche0142@gmail.com>
2020-09-01 18:18:15 +1000

    task2, task3, week 5

---
+++ Lab_Week5/task2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+int main(int argc, char* argv[])
+{
+    int my_rank;
+    int p;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
+    int val = -1;
+    do{
+        if(my_rank == 0){
+            printf("Enter a round number (> 0 ): ");
+            fflush(stdout);
+            scanf("%d", &val);
+        }
+        MPI_Bcast(&val, 1, MPI_INT, 0, MPI_COMM_WORLD); // This
+        function has to visible to all processes.
+        printf("Processors: %d. Received Value: %d\n", my_rank,
+        val);
+        fflush(stdout);
+    } while(val > 0);
+    MPI_Finalize();
+    return 0;
+}
---
+++ Lab_Week5/task3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <mpi.h>
+struct valuestruct {
+    int a;
+    double b;
+};
+int main(int argc, char** argv){
+    struct valuestruct values;
+    int myrank;
+    MPI_Datatype Valuetype;
+    MPI_Datatype type[2] = { MPI_INT, MPI_DOUBLE };
+    int blocklen[2] = { 1, 1};
+    MPI_Aint disp[2];
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+    MPI_Get_address(&values.a, &disp[0]);
+    MPI_Get_address(&values.b, &disp[1]);
+    //Make relative
+    disp[1]=disp[1]-disp[0];
+    disp[0]=0;
+    // Create MPI struct
+    MPI_Type_create_struct(2, blocklen, disp, type, &Valuetype);
+    MPI_Type_commit(&Valuetype);
+    do{
+        if (myrank == 0){
+            printf("Enter an round number (>0) & a real number: ");
+            fflush(stdout);
+            scanf("%d%lf", &values.a, &values.b);
+            }
+        MPI_Bcast(&values, 2, Valuetype, 0, MPI_COMM_WORLD);
+        printf("Rank: %d. values.a = %d. values.b = %lf\n",
+        myrank, values.a, values.b);
+        fflush(stdout);
+    } while(values.a > 0);
+    /* Clean up the type */
+    MPI_Type_free(&Valuetype);
+    MPI_Finalize();
+    return 0;
+}
