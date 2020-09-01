36982f8675d289c6d1ce7f872dbf61e819e20ae0
Ethan Nardella <enar0002@student.monash.edu>
2020-09-01 18:47:41 +1000

    Completed task 2

--- Lab_Week5/task1.c
+++ Lab_Week5/task1.c
@@ -3,12 +3,13 @@
 int main( argc, argv )
 int argc;
 char **argv;
+
 {
-    int rank, size;
-    MPI_Init( &argc, &argv );
-    MPI_Comm_size( MPI_COMM_WORLD, &size );
+    int rank, size;  // Initialise the variables for use
+    MPI_Init( &argc, &argv );   // Initialise MPI
+    MPI_Comm_size( MPI_COMM_WORLD, &size ); // Get the communicator size and rank for the current process
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-    printf( "Hello world from process %d of %d\n", rank, size );
-    MPI_Finalize();
+    printf( "Hello world from process %d of %d\n", rank, size ); // Print the information on this thread
+    MPI_Finalize();  // Stop MPI sending messages.
     return 0;
 }
--- Lab_Week5/task2.c
+++ Lab_Week5/task2.c
@@ -5,22 +5,20 @@ int main(int argc, char* argv[])
 {
     int my_rank;
     int p;
-    MPI_Init(&argc, &argv);
+    MPI_Init(&argc, &argv);  // Initialise MPI and get the rank/size values
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &p);
     int val = -1;
     do{
         if(my_rank == 0){
-            printf("Enter a round number (> 0 ): ");
+            printf("Enter a round number (> 0 ): ");  // Ask for the number from the user
             fflush(stdout);
-            scanf("%d", &val);
+            scanf("%d", &val);  
         }
-        MPI_Bcast(&val, 1, MPI_INT, 0, MPI_COMM_WORLD); // This
-        function has to visible to all processes.
-        printf("Processors: %d. Received Value: %d\n", my_rank,
-        val);
+        MPI_Bcast(&val, 1, MPI_INT, 0, MPI_COMM_WORLD); // This function has to be visible to all processes.
+        printf("Processors: %d. Received Value: %d\n", my_rank, val);
         fflush(stdout);
     } while(val > 0);
-    MPI_Finalize();
+    MPI_Finalize();  // Finish up with MPI and stop parallelism
     return 0;
 }
