59c524f5d92a171e5041d9baa46e18f9900e8aa4
Ethan Nardella <enar0002@student.monash.edu>
2020-09-15 19:09:56 +1000

    Partially implemented task 1

--- Lab_Week7/task1.c
+++ Lab_Week7/task1.c
@@ -8,11 +8,13 @@ int main( argc, argv )
 int argc;
 char **argv;
 {
-    int rank, value, size, errcnt, toterr, i, j;
+    int rank, value, size, errcnt, toterr, i, j, up_nbr, down_nbr;
     MPI_Status status;
     double x[12][12];
     double xlocal[(12/4)+2][12];
-    MPI_Init( &argc, &argv );
+    MPI_Request request[size * 2];
+    MPI_Status status[size * 2];
+    MPI_Init( &argc, &argv );  // Begin MPI
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
     MPI_Comm_size( MPI_COMM_WORLD, &size );
     if (size != 4) MPI_Abort( MPI_COMM_WORLD, 1 );
@@ -25,36 +27,65 @@ char **argv;
 	xlocal[0][j] = -1;
 	xlocal[maxn/size+1][j] = -1;
     }
-    /* Send up unless I'm at the top, then receive from below */
-    /* Note the use of xlocal[i] for &xlocal[i][0] */
-    if (rank < size - 1) 
-	MPI_Send( xlocal[maxn/size], maxn, MPI_DOUBLE, rank + 1, 0, 
-		  MPI_COMM_WORLD );
-    if (rank > 0)
-	MPI_Recv( xlocal[0], maxn, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, 
-		  &status );
-    /* Send down unless I'm at the bottom */
-    if (rank > 0) 
-	MPI_Send( xlocal[1], maxn, MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD );
-    if (rank < size - 1) 
-	MPI_Recv( xlocal[maxn/size+1], maxn, MPI_DOUBLE, rank + 1, 1, 
-		  MPI_COMM_WORLD, &status );
-    /* Check that we have the correct results */
+
+    /*
+      CALL MPI_COMM_RANK(comm, rank, ierr)
+    IF(rank.EQ.0) THEN
+        CALL MPI_ISEND(a(1), 10, MPI_REAL, 1, tag, comm, request, ierr)
+        **** do some computation ****
+        CALL MPI_WAIT(request, status, ierr)
+    ELSE
+        CALL MPI_IRECV(a(1), 15, MPI_REAL, 0, tag, comm, request, ierr)
+        **** do some computation ****
+        CALL MPI_WAIT(request, status, ierr)
+    END IF
+    */
+   
+    up_nbr = rank + 1;  // This prevents crashing from occurring if the current process is 0 or size - 1 (max)
+    down_nbr = rank - 1;
+    if (up_nbr >= size){
+        up_nbr = MPI_PROC_NULL;
+    }
+    if (down_nbr < 0){
+        down_nbr = MPI_PROC_NULL;
+    }
+    
+    // Send above / Send below
+    MPI_ISend( xlocal[maxn/size], maxn, MPI_DOUBLE, up_nbr, 0, MPI_COMM_WORLD, &request[rank*2] );
+    MPI_ISend( xlocal[1], maxn, MPI_DOUBLE, down_nbr 1, MPI_COMM_WORLD, &request[rank*2 + 1] );
+
+    MPI_Waitall(size*2, request);
+    // receive above / receive below
+    MPI_Recv( xlocal[maxn/size+1], maxn, MPI_DOUBLE, up_nbr, 1, MPI_COMM_WORLD, &status );
+    MPI_Recv( xlocal[0], maxn, MPI_DOUBLE, down_nbr, 0, MPI_COMM_WORLD, &status );
+
+
     errcnt = 0;
     for (i=1; i<=maxn/size; i++) 
-	for (j=0; j<maxn; j++) 
-	    if (xlocal[i][j] != rank) errcnt++;
-    for (j=0; j<maxn; j++) {
-	if (xlocal[0][j] != rank - 1) errcnt++;
-	if (rank < size-1 && xlocal[maxn/size+1][j] != rank + 1) errcnt++;
+	for (j=0; j<maxn; j++){
+	    if (xlocal[i][j] != rank){
+            errcnt++;
+        }
     }
+    for (j=0; j<maxn; j++) {
+	    if (xlocal[0][j] != rank - 1){
+            errcnt++;
+        }
+        if (rank < size-1 && xlocal[maxn/size+1][j] != rank + 1){
+            errcnt++
+        };
+    }
+
     MPI_Reduce( &errcnt, &toterr, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD );
     if (rank == 0) {
-	if (toterr)
-	    printf( "! found %d errors\n", toterr );
-	else
-	    printf( "No errors\n" );
+        if (toterr){
+            printf( "! found %d errors\n", toterr );
+        }
+        else{
+            printf( "No errors\n" );
+        }
     }
-    MPI_Finalize( );
+
+    MPI_Finalize();
     return 0;
 } 
