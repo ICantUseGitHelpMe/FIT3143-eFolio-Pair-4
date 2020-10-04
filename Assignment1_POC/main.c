e9782f91a4733a13b11d0de99d3b4a97649cf01a
Ethan Nardella <enar0002@student.monash.edu>
2020-10-04 15:59:12 +1100

    Implemented POC

--- Assignment1_POC/main.c
+++ Assignment1_POC/main.c
@@ -20,15 +20,17 @@
  *      - all processes will poll from the shared array 
  *        when reading / writing
  *
- * mpicc main.c -o main_out
+ * mpicc main.c -o main_out -lm
  * mpirun -np 4 main_out
  */
 
 #include <stdio.h>
 #include <mpi.h>
 #include <stdbool.h>
+#include <math.h>
 int set_memory(int index, int value);
 int get_memory(int index, int rank, int size);
+void finish(int rank);
 
 int main( argc, argv )
 int argc;
@@ -37,7 +39,7 @@ char **argv;
     int rank, size;
     int share_buff[100];  // The array for our "Distributed Shared Memory"
 
-    int goal = 100;  // Target we are summing to
+    int goal = 10;  // Target we are summing to
 
     // shared memory stuff here
 
@@ -46,37 +48,66 @@ char **argv;
     MPI_Comm_size( MPI_COMM_WORLD, &size );  // The number of processors
 
     if (rank == 0){
-        share_buff[0] = 27;
-        share_buff[1] = 1;
+        share_buff[0] = 0;
         bool exit[size];
         for (int j; j < size; j++){
             exit[j] = false;
         }
-        while (true)
+        bool processing = true;
+        while (processing)
         {   
             int buf[3];  // buf[0] has the code, index, and reply rank
             MPI_Recv(buf, 3, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             if (buf[0] == 20){  // Done
                 // Update the relevant process to complete
                 exit[buf[2]] = true;
+                // printf("Proc %d FInished!\n", buf[2]);
             }
-            if (buf[0] == 10){ // Information
+            else if (buf[0] == 10){ // Information
+                // printf("Value now: %d, sent val %d\n", share_buff[0],  share_buff[buf[1]]);
                 MPI_Send(&share_buff[buf[1]], 1, MPI_INT, buf[2], 0, MPI_COMM_WORLD);    // MPI_Send to the main thread
-
+                
+                int val[2];  // val[0] is the index, val[1] is the value
+                MPI_Recv(val, 2, MPI_INT, buf[2], MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                share_buff[val[0]] = val[1];
+                // printf("Value now: %d, RECV val %d\n", share_buff[0], val[1]);
+                
             }
+
+            // Check if we are ready to end by checking all processors
+            bool stop = true;
+            for (int check = 1; check < size; check ++){
+                stop = stop && exit[check]; // Only returns true if everything is false
+            }
+
+            processing = !stop;
             
         }
+
+        printf("Final result: %d\n", share_buff[0]);
         
     }   
     else{ 
-        for (int i = ((float) rank / size) * goal; i < ((float) (rank + 1) )/ size * goal; i++){
-            // int current = share_buff[0];
+
+        // Calculate the range for each processor
+        float relative_rank = rank - 1; // Account for rank 0 being the memory controller
+        float next_rel_rank = relative_rank + 1;
+        int relative_size = size - 1;
+        int cumulate = 0;
+
+        for (int i =  floor(relative_rank / relative_size * goal) + 1; i < ceil(next_rel_rank  / relative_size * goal); i++){
             int current = get_memory(0, rank, size);
-            printf("Process: %d,Val: %d\n", rank, current);
-            // int new_val = current + i;
-            // share_buff[0] = new_val;
-            // set_memory(0, new_val);  // Send the memory
+            int new_val = current + i;
+            set_memory(0, new_val);  // Send the memory
         }
+
+        if (rank == size - 1){  // The final processor can add the last number which would otherwise be an outlier
+            int current = get_memory(0, rank, size);
+            int new_val = current + goal;
+            set_memory(0, new_val);  // Send the memory
+        }
+
+        finish(rank);  // Send the finsihed signal
     }
     // individual process stuff here
 
@@ -100,6 +131,18 @@ int get_memory(int index, int rank, int size){
 }
 
 int set_memory(int index, int value){
+    int buf[2];
+    buf[0] = index;
+    buf[1] = value;
+    MPI_Send(buf, 2, MPI_INT, 0, 0, MPI_COMM_WORLD);    // MPI_Send to the main thread
+    return 1;
+}
 
-    return 0;
+void finish(int rank){
+    int buf[3];
+    buf[0] = 20;  // End code
+    buf[1] = -1; // Irrelevant
+    buf[2] = rank;  // The rank to end
+    MPI_Send(buf, 3, MPI_INT, 0, 0, MPI_COMM_WORLD);    // MPI_Send to the main thread
+    return;
 }
