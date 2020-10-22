a2c08dcc067fda43989bfba6b5736da190accb99
Ethan Nardella <enar0002@student.monash.edu>
2020-10-23 01:29:54 +1100

    Added makefile and began setting up some other stuff

--- Assignment2/ass2.c
+++ Assignment2/ass2.c
@@ -17,6 +17,11 @@
 #define MOD_DENOMINATOR 60.0f // The number the generated temperature is modded by.  Determines the upper range of the generated temperature
 #define MIN_TEMP 25.0f        // The baseline temperature.
 #define SATELLITE_CACHE 5     // The number of elements the satellite will keep in its memory
+#define ROWS 2                // The number of rows of nodes
+#define COLUMNS 2             // The number of columns of nodes
+#define SATELLITE_REQUEST 2   // The Tag used for requests to the satellite
+#define BASE_REQUEST 1        // The Tag used for the requests to the base
+#define SERVER_ID 0           // The rank of the server node
 // Server headers
 int server_control();
 void satellite();
@@ -41,7 +46,7 @@ int main(int argc, char **argv)
     srand((unsigned)time(NULL) + rank); // Seed the random, uniquely for each thread
 
     //Server Controls:
-    if (rank == 0)
+    if (rank == SERVER_ID)
     {
         server_control();
     }
@@ -61,53 +66,105 @@ int server_control()
 {
     pthread_t thread_id;
     pthread_create(&thread_id, NULL, (void *)satellite, NULL); // Activate the satellite
-    server();  // Activate the server
+    server();                                                  // Activate the server
     pthread_join(thread_id, NULL);
     return 0;
 }
 
 void satellite()
 {
-    
-    int index_count = 0;  // This keeps track of the index of the memory array
+    MPI_Request request;
+    int index_count = 0; // This keeps track of the index of the memory array
     int DEBUG = 0;
-    unsigned timestamp_array[SATELLITE_CACHE];  // Stores the previous N timestamps
-    float temperature_array[SATELLITE_CACHE];   // Stores the previous N temperatures (tmeperature 2 corresponds to timestamp 2, etc.)
-    int coordinate_array[SATELLITE_CACHE][2];   // Stores the previous N coordinates associated with the above information
+    unsigned timestamp_array[SATELLITE_CACHE]; // Stores the previous N timestamps
+    float temperature_array[SATELLITE_CACHE];  // Stores the previous N temperatures (tmeperature 2 corresponds to timestamp 2, etc.)
+    int coordinate_array[SATELLITE_CACHE][2];  // Stores the previous N coordinates associated with the above information
 
     // Will send with tag 1 and receive tag 0, to node 0 (us)
     printf("Satellite!\n");
+    float received_coordinate[2]; // index 0 is the "x", index 1 is the "y"
+    // Get ready to receive the request for data
+    MPI_Irecv(received_coordinate, 2, MPI_INT, SERVER_ID, SATELLITE_REQUEST, MPI_COMM_WORLD, &request);
     for (int counter = 0; counter < ITERATIONS; counter++)
     {
         float temperature = generate_temp();
-        if (temperature > THRESHOLD){
+        if (temperature > THRESHOLD)
+        {
             timestamp_array[index_count] = time(NULL);
             temperature_array[index_count] = temperature;
             coordinate_array[index_count][0] = DEBUG;
             coordinate_array[index_count][1] = DEBUG++;
-            index_count ++;
-            index_count = index_count % SATELLITE_CACHE;  // Make sure it doesn't go above the limit
+            index_count++;
+            index_count = index_count % SATELLITE_CACHE; // Make sure it doesn't go above the limit
         }
-        printf("Temperature: %f\n", temperature);
+        int got_request;
+        MPI_Test(&request, &got_request, MPI_STATUS_IGNORE);
+        if (got_request == 1) // A request for data was received
+        {
+            MPI_Request sent_info;  // New requests for send and receive
+
+            MPI_Request new_req;
+            // Check if the received coordinates are in the list
+            int found = 0;
+            for (int index = 0; index < SATELLITE_CACHE; index++)
+            {
+                if (received_coordinate[0] == coordinate_array[index][0] && received_coordinate[1] == coordinate_array[index][1])
+                {
+                    // If coords match, return a successful response
+                    printf("MATCH\n");
+                    found = 1;
+                    float send_buf[2];
+                    send_buf[0] = (float) timestamp_array[index]; // The timestamp (cast as a float for ease, it will become unsigned again later)
+                    send_buf[1] = temperature_array[index]; // The temperature info
+                    MPI_Isend(send_buf, 2, MPI_FLOAT, SERVER_ID, BASE_REQUEST, MPI_COMM_WORLD, &sent_info);
+                }
+            }
+            if (found == 0)
+            { // Wasn't found
+
+                float send_buf[2];
+                send_buf[0] = -1;  // An impossible value for the time, so demonstrates it isn't present
+                send_buf[1] = 0;
+                MPI_Isend(send_buf, 2, MPI_FLOAT, SERVER_ID, BASE_REQUEST, MPI_COMM_WORLD, &sent_info);
+            }
+
+            // printf("Test response %d\n", got_request);
+            // Now prepare to receive again
+            MPI_Irecv(received_coordinate, 2, MPI_INT, SERVER_ID, SATELLITE_REQUEST, MPI_COMM_WORLD, &new_req);  // Can;t be the same request, try an array
+        }
+        // printf("Temperature: %f\n", temperature);
         usleep(INTERVAL);
     }
 
-    char readable_timestamp[100];
-    for (int index = 0; index < SATELLITE_CACHE; index ++){
+    char readable_timestamp[50];
+
+    // Debug printing
+    for (int index = 0; index < SATELLITE_CACHE; index++)
+    {
         struct tm tolocal;
-        time_t cast_time = (time_t) timestamp_array[index];
+        time_t cast_time = (time_t)timestamp_array[index];
         tolocal = *localtime(&cast_time);
         strftime(readable_timestamp, sizeof(readable_timestamp), "%Y-%m-%d %H:%M:%S %Z", &tolocal);
         printf("Temperature: %f, Time: %s, Coords: %d, %d\n", temperature_array[index], readable_timestamp, coordinate_array[index][0], coordinate_array[index][1]);
     }
-
+    // MPI_Cancel(&new_req);  // Remove any outstanding sends
 }
 void server()
 {
     // Will send with tag 0 and receive tag 1, to node 0 (us)
-    for (int counter = 0; counter < 100; counter++)
+    for (int counter = 0; counter < ITERATIONS; counter++)
     {
-        printf("Server!\n");
+        int coords[2]; // Stores the cooreds of the fire
+        coords[0] = 2;
+        coords[1] = 4;
+        MPI_Send(coords, 2, MPI_INT, SERVER_ID, SATELLITE_REQUEST, MPI_COMM_WORLD);
+        float received[2]; // The returned info
+        MPI_Recv(received, 2, MPI_FLOAT, SERVER_ID, BASE_REQUEST, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if (received[0] > 0)
+        { // Found
+            printf("MAtch: %u, %f!\n", (unsigned)received[0], received[1]);
+        }
+        // printf("Server!\n");
         usleep(INTERVAL);
     }
 }
@@ -117,7 +174,7 @@ void server()
 // General code
 float generate_temp()
 {
-    float random = (float) rand() / SPLITTER;                  // random number, made into a float
+    float random = (float)rand() / SPLITTER;      // random number, made into a float
     float modded = fmod(random, MOD_DENOMINATOR); // Put it in the appropriate range
     return modded + MIN_TEMP;
 }
