002987278331cd3dc169536bdb805e89f1897374
Ethan Nardella <enar0002@student.monash.edu>
2020-10-22 23:03:43 +1100

    Created the satellite logs

--- Assignment2/ass2.c
+++ Assignment2/ass2.c
@@ -10,11 +10,13 @@
 #include <time.h>
 #include <unistd.h>
 #include <math.h>
-#define INTERVAL 100 * 1000   // How frequently the main processor will check for updates from nodes (in microseconds)
+#define ITERATIONS 1000       // The number of loops in the server and satellite
+#define INTERVAL 50 * 1000    // How frequently the main processor will check for updates from nodes (in microseconds)
 #define SPLITTER 157          // Divide random numbers by this to make them floats.  This number is a prime
 #define THRESHOLD 80.0f       // The temperature that evokes a positive response (degrees)
 #define MOD_DENOMINATOR 60.0f // The number the generated temperature is modded by.  Determines the upper range of the generated temperature
 #define MIN_TEMP 25.0f        // The baseline temperature.
+#define SATELLITE_CACHE 5     // The number of elements the satellite will keep in its memory
 // Server headers
 int server_control();
 void satellite();
@@ -46,7 +48,7 @@ int main(int argc, char **argv)
     // Node controls
     else
     {
-        // Philip, start your work here and reference task 1 and 2 of Lab 10
+        // Philip, start your work here, you can use task 1 and 2 of Lab 10 as inspiration
     }
 
     // Rejoin everything together again
@@ -59,20 +61,46 @@ int server_control()
 {
     pthread_t thread_id;
     pthread_create(&thread_id, NULL, (void *)satellite, NULL); // Activate the satellite
-    server();                                                  // Activate the server
+    server();  // Activate the server
     pthread_join(thread_id, NULL);
     return 0;
 }
 
 void satellite()
 {
+    
+    int index_count = 0;  // This keeps track of the index of the memory array
+    int DEBUG = 0;
+    unsigned timestamp_array[SATELLITE_CACHE];  // Stores the previous N timestamps
+    float temperature_array[SATELLITE_CACHE];   // Stores the previous N temperatures (tmeperature 2 corresponds to timestamp 2, etc.)
+    int coordinate_array[SATELLITE_CACHE][2];   // Stores the previous N coordinates associated with the above information
+
     // Will send with tag 1 and receive tag 0, to node 0 (us)
     printf("Satellite!\n");
-    for (int counter = 0; counter < 100; counter++)
+    for (int counter = 0; counter < ITERATIONS; counter++)
     {
-        printf("Temperature: %f\n", generate_temp());
+        float temperature = generate_temp();
+        if (temperature > THRESHOLD){
+            timestamp_array[index_count] = time(NULL);
+            temperature_array[index_count] = temperature;
+            coordinate_array[index_count][0] = DEBUG;
+            coordinate_array[index_count][1] = DEBUG++;
+            index_count ++;
+            index_count = index_count % SATELLITE_CACHE;  // Make sure it doesn't go above the limit
+        }
+        printf("Temperature: %f\n", temperature);
         usleep(INTERVAL);
     }
+
+    char readable_timestamp[100];
+    for (int index = 0; index < SATELLITE_CACHE; index ++){
+        struct tm tolocal;
+        time_t cast_time = (time_t) timestamp_array[index];
+        tolocal = *localtime(&cast_time);
+        strftime(readable_timestamp, sizeof(readable_timestamp), "%Y-%m-%d %H:%M:%S %Z", &tolocal);
+        printf("Temperature: %f, Time: %s, Coords: %d, %d\n", temperature_array[index], readable_timestamp, coordinate_array[index][0], coordinate_array[index][1]);
+    }
+
 }
 void server()
 {
