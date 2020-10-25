f630b84e8dd88cc7cb1e819fdd27a6d3820b2a31
Ethan Nardella <enar0002@student.monash.edu>
2020-10-25 22:54:26 +1100

    Implemented file output and formatting

--- Assignment2/ass2.c
+++ Assignment2/ass2.c
@@ -35,6 +35,7 @@ struct Sat_Cache // The structure of the satellite cache
 int server_control();
 void satellite(struct Sat_Cache *Cache);
 void server(struct Sat_Cache *Cache);
+void file_append(char *out);
 // Node headers
 
 // General headers
@@ -61,10 +62,17 @@ int main(int argc, char **argv)
     // Node controls
     else
     {
+        // READ ME
         // Philip, start your work here, you can use task 1 and 2 of Lab 10 as inspiration
-        // I need (at least) the reporting node x and y, and the timestamp in the messages you send
+        // I need (at least) the reporting node's x, y and the timestamp in the messages you send
         // Also you MAY want to consider using POSIX threads (pthreads, like what I've used) to make more threads
         // Otherwise we'll only have 4 nodes ever.
+
+        // Get the current time using this, otherwise we won't have miliseconds
+        // struct timespec t_spec;
+        // clock_gettime(CLOCK_REALTIME, &t_spec);
+        // unsigned long now = (t_spec.tv_nsec + t_spec.tv_sec*1e9) * 1e-6;  // This converts from nanoseconds to miliseconds
+        // the unsigned long will have the time, you may have to experiment to see what MPI has that represents this
     }
 
     // Rejoin everything together again
@@ -123,17 +131,31 @@ void server(struct Sat_Cache *Cache)
     // It will only read from the coordinate array, temperature array, and the timestamp array.
 
     printf("Server\n");
-    for (int counter = 0; counter < ITERATIONS; counter++)
-    {                     // This is the mainloop
+    for (int counter = 0; counter < ITERATIONS; counter++) // This is the mainloop
+    {
         usleep(INTERVAL); // Sleep for a set time
 
         // Now, check if there is any entry from the satellite that matches the node coords
         // To do this, loop backwards through the array starting from the current "head", then restarting when the end is reached
         // This ensures we take the latest first
-        printf("_______\n");
 
+        // TODO: Assign these variables through MPI_IRECV / RECV
         int node_x = 0; // The coords of the reporting node
         int node_y = 0;
+        float temperature = 86.3f;               // The temperatureo reported by the node
+        unsigned long timestamp = 1603625547246; // TODO: Mention the assumed clock sync between processes in report
+
+        // Get the current time in miliseconds
+        struct timespec t_spec;
+        clock_gettime(CLOCK_REALTIME, &t_spec);                            // Get the struct wiht the time in s and ns
+        unsigned long now = (t_spec.tv_nsec + t_spec.tv_sec * 1e9) * 1e-6; // Combine and convert into ms
+
+        // Convert time to a readable string
+        char readable_timestamp[50];
+        struct tm tolocal;
+        time_t cast_time = (time_t)timestamp;
+        tolocal = *localtime(&cast_time);
+        strftime(readable_timestamp, sizeof(readable_timestamp), "%Y-%m-%d %H:%M:%S %Z", &tolocal);
 
         int index = Cache->index;
         bool found_entry = false;
@@ -148,22 +170,51 @@ void server(struct Sat_Cache *Cache)
             if (Cache->coordinate_array[index][0] == node_x && Cache->coordinate_array[index][1] == node_y)
             {
                 // The node has a reported temperature in the memory cache of the satellite
-                // TODO: Replace with fileout
-                printf("Data: %d, %d.  %f.  %u\n", Cache->coordinate_array[index][0], Cache->coordinate_array[index][1], Cache->temperature_array[index], Cache->timestamp_array[index]);
+                // Make the timestamp readable
+                char sat_timestamp[50];
+                struct tm tolocal;
+                time_t cast_time = (time_t)Cache->timestamp_array[index];
+                tolocal = *localtime(&cast_time);
+                strftime(sat_timestamp, sizeof(sat_timestamp), "%Y-%m-%d %H:%M:%S %Z", &tolocal);
+
+                char buf[256];
+                sprintf(buf, "Message duration: %lums  |  X: %d  |  Y: %d  |  Temperature: %f  |  Node Timestamp: %s  \n  Satellite Temperature: %f  |  Satellite Timestamp: %s\n\n\n\n", now - timestamp, Cache->coordinate_array[index][0], Cache->coordinate_array[index][1], temperature, readable_timestamp, Cache->temperature_array[index], sat_timestamp);
                 found_entry = true;
-                break; // Don't continue, in case it finds an older (outdated) response that we don't want to use
+                file_append(buf); // Write to file
+                break;            // Don't continue, in case it finds an older (outdated) response that we don't want to use
             }
         } while (index != Cache->index);
 
         if (!found_entry)
         {
-            // TODO: Replace with fileout
-            printf("Satellite has no cached entry for this node!\n");
-        }
-        // TODO: Report the time difference between the node and this process.  Record the time at the start of the loop
 
+            char buf[256];
+            sprintf(buf, "Message duration: %lums  |  X: %d  |  Y: %d  |  Temperature: %f  |  Node Timestamp: %s  \n  Satellite Temperature: NOT FOUND  |  Satellite Timestamp: NOT FOUND\n\n\n\n", now - timestamp, node_x, node_y, temperature, readable_timestamp);
+
+            file_append(buf); // Write to file
+        }
     }
     Cache->process = false; // Tell the other thread to stop
+}
+//File output:
+void file_append(char *out)
+{
+    FILE *output;
+
+    char *outputFileBuffer;                        // Declare buffer for name of output file
+    outputFileBuffer = malloc(sizeof(char) * 256); // Allocate space for declared buffer
+    strcpy(outputFileBuffer, "");
+    snprintf(outputFileBuffer, sizeof(char) * 256, "reports.txt");
+    output = fopen(outputFileBuffer, "a");
+
+    if (output == NULL)
+    {
+        printf("ERROR");
+        exit(1);
+    }
+
+    fprintf(output, "%s", out);
+    fclose(output);
 }
 
 // Node code
